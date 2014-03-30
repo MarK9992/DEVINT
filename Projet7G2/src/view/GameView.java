bfd9d2a5d7efa219d25ae09f31be635ef131f23f
Marc KARASSEV <karassev.marc@etu.unice.fr>
2014-03-30 20:36:20 +0200

    DEVINT#4

---
+++ Projet7G2/src/view/GameView.java
@@ -0,0 +1,14 @@
+package view;
+
+import map.GameMap;
+/**
+ * Created by Marc KARASSEV on 24/03/14.
+ * @author Marc KARASSEV
+ */
+public class GameView {
+    private GameMap gm;
+
+    public GameView() {
+        //TODO
+    }
+}
