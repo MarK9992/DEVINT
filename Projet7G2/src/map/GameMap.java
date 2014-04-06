84c4422c3ab1337c2b7c5f758f54fe622f52b1de
Marc KARASSEV <karassev.marc@etu.unice.fr>
2014-04-06 16:16:07 +0200

    DEVINT#15

--- Projet7G2/src/map/GameMap.java
+++ Projet7G2/src/map/GameMap.java
@@ -64,7 +64,7 @@ public class GameMap {
     public void moveHeroUp() {
         int speed = -DEFAULTSPEED;
 
-        if (Game.toTile(hero.getY() + speed) == -1) {
+        if (Game.toTile(hero.getUpLeftCornerY() + speed) == -1) {
             switchDivisionUp();
         } else {
             while (isUpTileAccessible(speed) && speed != 0) {
@@ -81,7 +81,7 @@ public class GameMap {
     public void moveHeroLeft() {
         int speed = -DEFAULTSPEED;
 
-        if (Game.toTile(hero.getX() + speed) == -1) {
+        if (Game.toTile(hero.getUpLeftCornerX() + speed) == -1) {
             switchDivisionLeft();
         } else {
             while (isLeftTileAccessible(speed) && speed != 0) {
@@ -98,7 +98,7 @@ public class GameMap {
     public void moveHeroRight() {
         int speed = DEFAULTSPEED;
 
-        if (Game.toTile(hero.getX() + hero.getWidth() + speed) == -1) {
+        if (Game.toTile(hero.getUpRightCornerX() + speed) == Game.XTILEMAX) {
             switchDivisionRight();
         } else {
             while (isRightTileAccessible(speed) && speed != 0) {
@@ -115,7 +115,7 @@ public class GameMap {
     public void moveHeroDown() {
         int speed = DEFAULTSPEED;
 
-        if (Game.toTile(hero.getY() + hero.getHeight() + speed) == Game.YTILEMAX) {
+        if (Game.toTile(hero.getDownLeftCornerX() + speed) == Game.YTILEMAX) {
             switchDivisionDown();
         } else {
             while (isDownTileAccessible(speed) && speed != 0) {
--- Projet7G2/src/map/MapDivision.java
+++ Projet7G2/src/map/MapDivision.java
@@ -53,7 +53,6 @@ public class MapDivision {
     }
 
     private void updateAccessMap(GameObject obj) {
-        System.out.println(Game.toTile(obj.getX()) + " " + Game.toTile(obj.getY()) + " " + Game.toTile(obj.getX() + obj.getWidth()) + " " + Game.toTile(obj.getY() + obj.getHeight()));
         for (int i = Game.toTile(obj.getX()); i < Game.toTile(obj.getX() + obj.getWidth()); i++) {
             for(int j = Game.toTile(obj.getY()); j < Game.toTile(obj.getY() + obj.getHeight()); j++) {
                 accessMap[i][j] = false;
