818d1893c1807c65192f427d76e754a176ea4f2a
MarK9992 <vasskera@hotmail.fr>
2014-05-17 12:30:12 +0200

    DEVINT#22 Gestion de la direction droite des sprites. Complétion des directions pour les darks et angel slimes.

--- Projet7G2/src/objects/Sprite.java
+++ Projet7G2/src/objects/Sprite.java
@@ -7,7 +7,7 @@ import org.newdawn.slick.Color;
  *
  * Each sprite has four images related to their movement direction.
  * The coordinate and dimensions of these images in the sheet are stored in the fields COORD and DIM.
- * The lignes of these matrixes match to an image, they are ordered by the sprite's direction : SOUTH, WEST and NORTH.
+ * The lignes of these matrixes match to an image, they are ordered by the sprite's direction : SOUTH, WEST, NORTH, and EAST.
  * The first column is the x location for COORD or the width for DIM, the second column is the y location or the height.
  *
  * @author Marc KARASSEV
@@ -23,8 +23,8 @@ public enum Sprite {
 
     // Fields
 
-    private String sheet;
-    private final short[][] COORD = new short[3][2], DIM = new short[3][2];
+    private String sheet, reverseSheet;
+    private final short[][] COORD = new short[4][2], DIM = new short[4][2];
     public static final Color TRANSP = new Color(34, 177, 76);
 
     // Constructors
@@ -33,18 +33,22 @@ public enum Sprite {
         switch(type) {
             case 0:
                 sheet = "../ressources/pic/dark slimes.png";
+                reverseSheet = "../ressources/pic/dark slimes2.png";
                 setDarkMatrixes();
                 break;
             case 1:
                 sheet = "../ressources/pic/angel slimes.png";
+                reverseSheet = "../ressources/pic/angel slimes2.png";
                 setAngelMatrixes();
                 break;
             case 2:
                 sheet = "../ressources/pic/fang slimes.png";
+                reverseSheet = "../ressources/pic/fang slimes2.png";
                 setFangMatrixes();
                 break;
             case 3:
                 sheet = "../ressources/pic/slimes.png";
+                reverseSheet = "../ressources/pic/slimes2.png";
                 setSlimeMatrixes();
                 break;
             default:
@@ -60,19 +64,35 @@ public enum Sprite {
         COORD[1][1] = 35;
         COORD[2][0] = 70;
         COORD[2][1] = 1;
+        COORD[3][0] = 193;
+        COORD[3][1] = 35;
         DIM[0][0] = 36;
         DIM[0][1] = 29;
         DIM[1][0] = 36;
         DIM[1][1] = 29;
         DIM[2][0] = 36;
         DIM[2][1] = 31;
+        DIM[3][0] = 36;
+        DIM[3][1] = 29;
     }
 
     private void setAngelMatrixes() {
-        COORD[0][0] = 0;
-        COORD[0][1] = 62;
-        DIM[0][0] = 38;
-        DIM[0][1] = 29;
+        COORD[0][0] = 1;
+        COORD[0][1] = 63;
+        COORD[1][0] = 1;
+        COORD[1][1] = 33;
+        COORD[2][0] = 1;
+        COORD[2][1] = 1;
+        COORD[3][0] = 270;
+        COORD[3][1] = 33;
+        DIM[0][0] = 36;
+        DIM[0][1] = 27;
+        DIM[1][0] = 36;
+        DIM[1][1] = 27;
+        DIM[2][0] = 36;
+        DIM[2][1] = 29;
+        DIM[3][0] = 36;
+        DIM[3][1] = 27;
     }
 
     private void setFangMatrixes() {
@@ -109,6 +129,11 @@ public enum Sprite {
         return coords;
     }
 
+    public short[] getEastCoords() {
+        short[] coords = {COORD[3][0], COORD[3][1]};
+        return coords;
+    }
+
     public short[] getSouthDims() {
         short[] dims = {DIM[0][0], DIM[0][1]};
 
@@ -127,9 +152,17 @@ public enum Sprite {
         return dims;
     }
 
+    public short[] getEastDims() {
+        short[] dims = {DIM[3][0], DIM[3][1]};
+
+        return dims;
+    }
+
     // Getters and setters
 
     public String getSheet() {
         return sheet;
     }
+
+    public String getReverseSheet() { return reverseSheet; }
 }
