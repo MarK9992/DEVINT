3da8baba4d2f95d2afcae260a71d9f5abe08a2a8
Tom Veniat <veniat.tom@etu.unice.fr>
2014-05-19 22:55:40 +0200

    nouveau objectifs

--- Projet7G2/src/map/MapCreator.java
+++ Projet7G2/src/map/MapCreator.java
@@ -1,45 +1,125 @@
 package map;
 
+import level.Direction;
+import level.ObjectiveGestion;
 import main.Game;
 import objects.ObjectType;
 import objects.Rock;
+import states.MainGameState;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Tom Veniat on 16/05/2014.
  */
 public class MapCreator {
-    private int width, height;
+    private int width, height, firstX, firstY;
     private int[][] accessMap;
-    MapDivision[][] map ;
+    private MapDivision[][] map ;
+    private ObjectiveGestion objective;
 
     public MapCreator(){
-        width=2;
-        height=2;
-        map = new MapDivision[width][height];
+
     }
 
     public void initialize(GameMap game) {
 
-        game.setMapWidth(width);
-        game.setMapHeight(height);
-
         try {
             MapParser parser=new MapParser();
-            accessMap=parser.getMap();
+            width=parser.getGameWidth();
+            height=parser.getGameHeight();
 
+            accessMap=parser.getMap();
+            firstX=parser.getFirstX();
+            firstY=parser.getFirstY();
+
+            objective=createObjective(parser.getObjective());
+
+            map = new MapDivision[width][height];
             for(int j=0;j<height;j++){
                 for (int i=0;i<width;i++){
                     map[i][j]=new MapDivision(accessMap[j*width+i]);
                 }
             }
         } catch (Exception e) {
+            width=4;
+            height=4;
+            map = new MapDivision[width][height];
             createHardcoreMap();
+            createHardcoreObjective();
+            firstX=1;
+            firstY=2;
+            e.printStackTrace();
         }
 
-        game.setMap(map);
+        initializeGame(game);
+
     }
 
+    private ObjectiveGestion createObjective(int[] objInt) {
 
+        Direction[] objDir=toDirectionArray(objInt);
+        if (isValidPath(objDir)){
+            return new ObjectiveGestion(toList(objDir));
+        }else {
+            throw new IllegalArgumentException("Bad objective List");
+        }
+
+    }
+
+    private boolean isValidPath(Direction[] objDir) {
+        boolean goodPath=true;
+        int step=0,currentX=firstX,currentY=firstY;
+        Direction dir;
+        while (goodPath){
+            if (step==objDir.length)
+                return true;
+            else{
+                dir=objDir[step++];
+                currentX+=dir.getMoveX();
+                currentY+=dir.getMoveY();
+                goodPath= (currentX>=0) && (currentY>=0) && (currentX<width) && (currentY<height);
+            }
+
+        }
+        return false;
+    }
+
+    private List<Direction> toList(Direction[] objDir) {
+        List<Direction> directionList=new ArrayList<Direction>();
+        for (Direction d :objDir){
+            directionList.add(d);
+        }
+        return directionList;
+    }
+
+    private Direction[] toDirectionArray(int[] objInt) {
+        Direction[] result = new Direction[objInt.length];
+        for (int i=0; i<objInt.length;i++){
+            result[i]=Direction.valueOf(objInt[i]);
+        }
+        return result;
+    }
+
+    private void initializeGame(GameMap game) {
+        game.setMapWidth(width);
+        game.setMapHeight(height);
+        game.setMap(map);
+        game.setMapi(firstX);
+        game.setMapj(firstY);
+        game.setObjective(objective);
+    }
+
+    private void createHardcoreObjective() {
+        ArrayList<Direction> list=new ArrayList<Direction>();
+        list.add(Direction.UP);
+        list.add(Direction.LEFT);
+        list.add(Direction.UP);
+        list.add(Direction.RIGHT);
+        list.add(Direction.DOWN);
+        this.objective=new ObjectiveGestion(list);
+    }
 
 
     private void createHardcoreMap() {
@@ -171,4 +251,5 @@ public class MapCreator {
         map[3][3] = div;
 
     }
+
 }
